38bb2704c7ad331dc9767ae9fa4b5631a3793d36
izuua <izuuaaf@gmail.com>
2019-08-15 01:11:54 -0400

    Added the functionality to create a burger.

--- models/burger.js
+++ models/burger.js
@@ -5,7 +5,14 @@ var burger = {
         orm.selectAll('burgers', function(res) {
             cb(res);
         })
-    }
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, function(res) {
+            cb(res);
+        })
+    },
 }
+
+//updateOne()
 
 module.exports = burger;
