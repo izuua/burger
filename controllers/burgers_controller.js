8aa3b9b2a8340270de458bebe28bca8adff776f2
izuua <izuuaaf@gmail.com>
2019-08-15 22:20:48 -0400

    Added the devour function.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -25,4 +25,21 @@ router.post('/api/burgers', function(req, res) {
     })
 })
 
+router.put('/api/burgers/:id', function(req, res) {
+    var condition = `id = ${req.params.id}`;
+
+    console.log(`condition ${condition}`);
+    console.log(req.body);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
+
 module.exports = router;
